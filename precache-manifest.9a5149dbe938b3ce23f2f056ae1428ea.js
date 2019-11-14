6f32ce26f19f8e20deb8b4d34c5031500f1dd264
idan <idanelbaz@gmail.com>
2019-11-14 14:28:01 +0200

    Updates

---
+++ precache-manifest.9a5149dbe938b3ce23f2f056ae1428ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7676862fbae17008892b8837bace8a37",
+    "url": "/user-map/index.html"
+  },
+  {
+    "revision": "98ed7b83ea6b7d1568cb",
+    "url": "/user-map/static/css/2.a94142c2.chunk.css"
+  },
+  {
+    "revision": "16b6af4e22511e2e3a8e",
+    "url": "/user-map/static/css/main.fbac5261.chunk.css"
+  },
+  {
+    "revision": "98ed7b83ea6b7d1568cb",
+    "url": "/user-map/static/js/2.eee8871a.chunk.js"
+  },
+  {
+    "revision": "16b6af4e22511e2e3a8e",
+    "url": "/user-map/static/js/main.63bdf49b.chunk.js"
+  },
+  {
+    "revision": "15dc5c17faf809130d1e",
+    "url": "/user-map/static/js/runtime~main.0e946c79.js"
+  }
+]);
